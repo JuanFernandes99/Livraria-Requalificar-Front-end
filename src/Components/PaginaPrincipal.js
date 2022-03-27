4d9a17c00e321123ddc1edb059b65ddac54648b4
jbarrinhos <96847584+jbarrinhos@users.noreply.github.com>
2022-03-27 23:54:35 +0100

    ...

--- src/Components/PaginaPrincipal.js
+++ src/Components/PaginaPrincipal.js
@@ -1,19 +1,24 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
 import livraryimage from "./livraria.png";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
 const API_URL = "http://localhost:8080";
 
 export function PaginaPrincipal() {
- // return <h1>Sem livros disponíveis</h1>;
+  // return <h1>Sem livros disponíveis</h1>;
   const [listaLivros, setListasLivros] = useState([]);
-
 
   useEffect(() => {
     fetchLivro();
@@ -45,23 +50,33 @@ export function PaginaPrincipal() {
       });
   }
 
-
   return (
     <>
-      <section className="list-container">
-        {listaLivros.map(function (element, index) {
-          return (
-            <div key={index} className="todo-card">
-             <img id="image" src={livraryimage} alt="Logo" />
-             <br></br>
-                { element.titulo} <br></br> {"" + element.preco + "$"}
-              
-            </div>
-          );
-        })}
-      </section>
-
-
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+      ); }
     </>
   );
 }
