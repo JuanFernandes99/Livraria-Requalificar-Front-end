340b577fc14c2533411e64f09501b17a4c51c799
jbarrinhos <96847584+jbarrinhos@users.noreply.github.com>
2022-04-03 21:08:45 +0100

    uptade funcionario.

--- src/Components/Funcionario/RegistoFuncionario.js
+++ src/Components/Funcionario/RegistoFuncionario.js
@@ -10,11 +10,17 @@ import livraryimage from "../Images/livraria.png";
 import { useState } from "react";
 import "./LoginFuncionario";
 import "../Geral/Adicionar.css";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import * as React from "react";
 
 const theme = createTheme();
+
 const API_URL = "http://localhost:8080";
 
 export function RegistoFuncionario() {
+  const [value, setValue] = React.useState(null);
   const navigate = useNavigate();
   const [novoFuncionario, setnovoFuncionario] = useState({
     nome: "",
@@ -124,6 +130,21 @@ export function RegistoFuncionario() {
                 });
               }}
             />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Data Nascimento"
+                inputFormat="dd/MM/yyyy"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                  setnovoFuncionario({
+                    ...novoFuncionario,
+                    dataNascimento: newValue,
+                  });
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
             <Button
               id="ButtonLogin"
               onClick={() => {
